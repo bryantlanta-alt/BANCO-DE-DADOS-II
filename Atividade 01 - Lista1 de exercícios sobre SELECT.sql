8898d1ec66f9574bc1afd157b1024f842b205ba3
bryantlanta-alt <bryantlanta@gmail.com>
2026-08-21 16:54:36 -0300

    Correcao da atividade 1

--- Atividade 01 - Lista1 de exercícios sobre SELECT.sql
+++ Atividade 01 - Lista1 de exercícios sobre SELECT.sql
@@ -1,42 +1,56 @@
 USE BancoPedidos;
 
+-- 1 
 SELECT DescricaoProduto 
 FROM produto 
 WHERE Unidade = 'M' AND ValUnit = 1.05;
 
+-- 2
 SELECT Nome, CEP 
 FROM cliente 
-WHERE CEP BETWEEN '30077500' AND '30079000';
+WHERE CEP BETWEEN 30077500 AND 30079000
+ORDER BY CEP ASC;
 
+-- 3
 SELECT MIN(SalarioFixo) AS MenorSalario, MAX(SalarioFixo) AS MaiorSalario 
 FROM vendedor;
+
+-- 4
+SELECT *
+FROM ITEMDOPEDIDO;
 
 SELECT SUM(i.Quantidade) AS "Quantidade Total"
 FROM itemdopedido i
 JOIN produto p ON i.CodigoProduto = p.CodProduto
 WHERE p.DescricaoProduto = 'Vinho';
 
-SELECT NumPedido, COUNT(CodigoProduto) AS "Qtos Produtos Diferentes"
+-- 5
+SELECT NumPedido, COUNT(*) AS QTOSPRODUTOSDIFERENTES
 FROM itemdopedido
 GROUP BY NumPedido
 HAVING COUNT(CodigoProduto) > 3;
 
+-- 6
 SELECT * 
 FROM cliente 
 WHERE Cidade = 'Niteroi';
 
+-- 7
 SELECT NumPedido, CodigoProduto, Quantidade 
 FROM itemdopedido 
 WHERE Quantidade = 35;
 
-SELECT NomeVendedor AS "Nome Vendedor" 
+-- 8
+SELECT NomeVendedor
 FROM vendedor 
 WHERE NomeVendedor NOT LIKE 'Jo%';
 
+-- 9
 SELECT Nome 
 FROM cliente 
 WHERE IE IS NULL;
 
-SELECT NomeVendedor AS "Nome Vendedor" 
+-- 10
+SELECT NOMEVENDEDOR
 FROM vendedor 
 WHERE FaixaComissao IN ('A', 'B');
